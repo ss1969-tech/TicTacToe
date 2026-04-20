b19c33b6a1625eb45c7671de8fc28f6e0642611d
ss1969-tech <ss1969@srmist.edu.in>
2026-04-20 12:52:32 +0530

    usecase1

---
+++ UC1TicTacToe.java
@@ -0,0 +1,22 @@
+public class UC1TicTacToe {
+    public static void main(String[] args) {
+        char[][] board = new char[3][3];
+
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                board[row][col] = '-';
+            }
+        }
+
+        printBoard(board);
+    }
+
+    public static void printBoard(char[][] board) {
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
